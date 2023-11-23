1b02a9b93bdb980c30479b0cadd74ab89f79f84d
chiragZechrome <134510344+chiragZechrome@users.noreply.github.com>
2023-11-23 17:18:46 +0530

    Create HelloWorldComponent.js

---
+++ src/HelloWorldComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorldComponent from './HelloWorldComponent'; // Assuming the file is in the same directory
+
+function App() {
+  return (
+    <div>
+      <HelloWorldComponent />
+    </div>
+  );
+}
+
+export default App;
